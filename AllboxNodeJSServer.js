cdda92137e213765eb11198b9a74a8d009d48d53
mtolstykh <maxim.v.tolstyh@gmail.com>
2014-08-06 12:32:33 +0400

    fix

--- AllboxNodeJSServer.js
+++ AllboxNodeJSServer.js
@@ -77,7 +77,7 @@ webSocketServer.on('connection', function (ws) {
             if (clients[userId] != undefined) {
                 clients[userId].send("Allboxx: " + message);
             }
-            db.updateUser(user, function (err, res) {
+            db.updateUser(users[userId], function (err, res) {
                 console.log(res);
             });
         } else if (utils.strsta(message, "user:new:")) {
